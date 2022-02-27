89a92b5764636facc853ca829672f7a67840c87d
Francisco Bórquez <fborquez@outlook.com>
2022-02-26 22:38:12 -0300

    feat: logic and poc into separate projects

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var demoRouter = require('./routes/demo');
-var setHttpPolicyHeader = require('./lib/Middleware/setHttpPolicyHeader');
+var setHttpPolicyHeader = require('@fjborquez/sethttppoliciesmiddleware');
 
 var coepOptions = {
   policy: 'coep',
